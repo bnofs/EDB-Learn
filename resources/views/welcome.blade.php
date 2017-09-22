d3b605cb9d7930d8a1bc3ac49e4c9bef733cf8de
Brendan Nofs <nofsbm@miamioh.edu>
2017-09-22 12:08:52 -0400

    Main page updated with search bar and scroll for explore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,9 @@
                 height: 100vh;
                 margin: 0;
             }
-
+            .main {
+              height: 125%;
+            }
             .full-height {
                 height: 100vh;
             }
@@ -29,6 +31,14 @@
                 align-items: center;
                 display: flex;
                 justify-content: center;
+            }
+            #explore {
+              padding-top: 50px;
+            }
+
+            h2{
+              font-size: 40px;
+              text-align: center;
             }
 
             .position-ref {
@@ -61,14 +71,82 @@
             .links > a:hover{
               background-color: lightgrey;
               color: white;
+              cursor: pointer;
             }
 
             .m-b-md {
                 margin-bottom: 30px;
             }
+            /* The Modal (background) */
+            .modal {
+                display: none; /* Hidden by default */
+                position: fixed; /* Stay in place */
+                z-index: 1; /* Sit on top */
+                left: 0;
+                top: 0;
+                width: 100%; /* Full width */
+                height: 100%; /* Full height */
+                overflow: auto; /* Enable scroll if needed */
+                background-color: rgb(0,0,0); /* Fallback color */
+                background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+            }
+
+            /* Modal Content/Box */
+            .modal-content {
+                background-color: #fefefe;
+                margin: 15% auto; /* 15% from the top and centered */
+                padding: 20px;
+                border: 1px solid #888;
+                width: 80%; /* Could be more or less, depending on screen size */
+            }
+
+            /* The Close Button */
+            .close {
+                color: #aaa;
+                float: right;
+                font-size: 28px;
+                font-weight: bold;
+            }
+
+            .close:hover,
+            .close:focus {
+                color: black;
+                text-decoration: none;
+                cursor: pointer;
+            }
+            #myInput {
+            width: 90%; /* Full-width */
+            font-size: 20px; /* Increase font-size */
+            padding: 12px 20px 12px 20px; /* Add some padding */
+            border: none; /* Add a grey border */
+            }
+
+            #myUL {
+                /* Remove default list styling */
+                list-style-type: none;
+                padding: 0;
+                margin: 0;
+            }
+
+            #myUL li a {
+                border: 1px solid #ddd; /* Add a border to all links */
+                margin-top: -1px; /* Prevent double borders */
+                background-color: #f6f6f6; /* Grey background color */
+                padding: 12px; /* Add some padding */
+                text-decoration: none; /* Remove default text underline */
+                font-size: 18px; /* Increase the font-size */
+                color: black; /* Add a black text color */
+                display: block; /* Make it into a block element to fill the whole list */
+            }
+
+            #myUL li a:hover:not(.header) {
+                background-color: #eee; /* Add a hover effect to all links, except for headers */
+            }
         </style>
     </head>
     <body>
+      <div class="main">
+
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -86,20 +164,87 @@
                     Ethnobotanical Database
                 </div>
                 <div class="links">
-                    <a href="#browse">Browse</a>
-                    <a href="#search">
+                    <a href="#explore">Explore</a>
+                    <a id="searchButton">
                       <i class="fa fa-search"></i> Search
                     </a>
                 </div>
             </div>
         </div>
 
-        <div id="browse">
+        <div id="searchBox" class="modal">
+
+          <!-- Modal content -->
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <i class="fa fa-search" aria-hidden="true"></i>
+            <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search Database...">
+
+
+          </div>
 
         </div>
-        <div id="search">
 
+      </div>
+
+        <div class="main" id="explore">
+          <h2>Explore</h2>
         </div>
 
     </body>
+    <script>
+    // Get the modal
+    var modal = document.getElementById('searchBox');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("searchButton");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+    $(document).ready(function(){
+      // Add smooth scrolling to all links
+      $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+    });
+    </script>
 </html>
