88c25f0ee1d84e81598a33822886ca298f8a662e
bnofs <bnofs13@gmail.com>
2017-10-04 18:26:43 -0400

    Add files via upload

---
+++ learn.blade.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Ethnobotanical Database</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">        <!-- Styles -->
+        <link href='https://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    </head>
+    <body>
+      <div id="home">
+
+      </div>
+      <ul class="navbar">
+
+        <li class="navitem"><a href="/">Home</a></li>
+        <li class="active navitem"><a href="/learn">Food</a></li>
+        <li class="navitem"><a href="/learn">Medicine</a></li>
+        <li class="navitem"><a href="/learn">Materials</a></li>
+        <li class="navitem"><a href="/learn">Customs</a></li>
+        <li class="navitem"><a href="/learn">Technology</a></li>
+
+      </ul>
+
+      <div class="main">
+
+        <div class="flex-center position-ref half-height">
+
+            <div class="content">
+
+                <div class="title m-b-md">
+                    Learn
+                </div>
+
+            </div>
+
+        </div>
+        <div class="row">
+          <div class="box col-lg-10 col-lg-offset-1">
+            <div class="col-lg-4">
+              <h2>Items</h2>
+              <hr />
+                <h4 class="clicked">Wild Potato</h4>
+                <h4>Onions</h4>
+                <h4>Mushroom</h4>
+                <h4>Sugar Maple Tree</h4>
+                <h4>Garlic</h4>
+                <h4>Ground Nut</h4>
+                <h4>Wild Ginger</h4>
+            </div>
+
+            <div class="col-lg-8" id="filter-box">
+              <h2>Details</h2>
+              <hr />
+              <h2>Wild Potato</h2>
+              <br/>
+              <div class="col-lg-4">
+                <img src="img/potatoes.jpg" />
+              </div>
+              <p>1. tubers eaten. "These are found in abundance in the wet prairies throughout the country and are gathered in "hoeing time" (June). They are easily boiled and when they wish to change colour the Indians throw into the kettle a few leaves of the soft maple, which turns them black" (Trowbridge 1825, from Kinietz 1938:63-64);
+                <br/><br/>
+                2. but refers to adding leaves of soft maple to a batch of boiling wild potatoes to turn them black (Dunn ca. 1900); 3. FOOD: the Miami-Illinois term 'ahpena' means potatoes, and indicated a number of species growing in the western prairies. "This "pomme blance" as named by the "coureurs des bois" look dark and are sweeter than our Irish potatoes. They cook in six hours and are usually called by these Indians nalau'xki pa<'nacki" or nalaaohki ahpena, wild potato (em p 359);
+                <br/><br/>3. "I found some roots, potatoes, for example digging with a stick etc." (Gravier ca.1700:291.15, "j ay trouve  des racines, pommes de terre, 8g fouissant avec un boic &c.");
+                <br/><br/>4. tubers cooked. "waap-", "It is easily cooked and is considered very good", resembling the cultivated potato in appearance and taste, found growing in the low prairies (Trowbridge 1825, from Kinietz 1938:65)[may refer to a different species];
+                <br/><br/>5. tubers boiled in water by a slow fire, and then eaten. :"That country [lands around Lake Michigan] also produces potatoes, some are as large as an egg, others have the size of one's fist, or a little more. They boil these in water by a slow fire during twenty-four hours, when they are thoroughly cooked, you will find in them an excellent flavor, much resembling that of prunes" (Perrot 1680-1718, translation from French to English by Blair 1911:117, Perrot's description may refer to Psoralea esculenta, Helianthus tuberosa or another species producing sizeable tubers, this ref. also published by Dunn ca. 1900);
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+        </div>
+
+
+</html>
